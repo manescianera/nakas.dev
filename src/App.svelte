adf556e4c1f06ffbeef24f6b405bcd660fa157c4
mcn <root@nakas.dev>
2022-01-13 20:18:01 +0200

    Added: routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,19 @@
 <script>
-	import { Router, Route } from "svelte-routing";
-	import Home from "./pages/Home.svelte"
-	export let url = "";
+	import router from "page"
+	import Home from "./components/Home.svelte"
+	import News from "./components/News.svelte"
+
+	let page
+
+	router('/', () => page = Home)
+	router('/news', () => page = News)
+
+	router.start()
 </script>
+
 
 <main>
 	<div id="content">
-		<Router {url}>
-			<Route basepath component={Home} />
-		</Router>
+		<svelte:component this={page} />
 	</div>
 </main>
