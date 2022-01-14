a82a59c7bb9ac8907afb4393525aea3a7d1a73f0
mcn <root@nakas.dev>
2022-01-14 12:48:23 +0200

    Changed: formatting; Added: list items

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,38 +1,38 @@
 <script>
-	let message = "meanwhile.."
-	let href = "https://github.com/manescianera"
+  let message = "meanwhile.."
+  let href = "https://github.com/manescianera"
 </script>
 
 <a {href}>{message}</a>
 
 <style>
-a {
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
+  a {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 
-	opacity: 1;
-	text-decoration: none;
-	font-size: calc(0.75em + 1vw);
-	color: #bcc5d8;
-	border-radius: 3px;
-	padding: 1rem;
-	animation: appear 2000ms;
-}
+    opacity: 1;
+    text-decoration: none;
+    font-size: calc(0.75em + 1vw);
+    color: var(--global-color-white);
+    border-radius: var(--global-border-radius);
+    padding: var(--global-padding);
+    animation: appear 2000ms;
+  }
 
-a:hover {
-	color: #bf616a;
-	background: #232835;
-	text-decoration: underline;
-}
+  a:hover {
+    color: var(--global-color-red);
+    background: var(--global-color-bg-secondary);
+    text-decoration: underline;
+  }
 
-@keyframes appear {
-	from {
-		opacity: 0;
-	}
-	to {
-		opacity: 1;
-	}
-}
+  @keyframes appear {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
 </style>
--- src/components/News.svelte
+++ src/components/News.svelte
@@ -1,14 +1,44 @@
 <script>
   import { onMount } from "svelte"
 
+  const date = Date.parse(new Date().toISOString()) / 1000
+  const title = "top"
   const api = "https://hacker-news.firebaseio.com/v0"
 
+  const getAgo = (time) => {
+    let diffInMilliSeconds = Math.abs(date - time)
+
+    const days = Math.floor(diffInMilliSeconds / 86400)
+    diffInMilliSeconds -= days * 86400
+
+    const hours = Math.floor(diffInMilliSeconds / 3600) % 24
+    diffInMilliSeconds -= hours * 3600
+
+    const minutes = Math.floor(diffInMilliSeconds / 60) % 60
+    diffInMilliSeconds -= minutes * 60
+
+    let diff = ""
+    if (days) {
+      diff += days === 1 ? `${days} day ` : `${days} days `
+    }
+
+    if (hours) {
+      diff += hours === 1 ? `${hours} hour ` : `${hours} hours `
+    }
+
+    if (!days && !hours && minutes) {
+      diff += minutes === 1 ? `${minutes} minute` : `${minutes} minutes`
+    }
+
+    return diff
+  }
+
   async function fetchIds() {
-    return fetch(`${api}/topstories.json`).then(x => x.json())
+    return fetch(`${api}/topstories.json`).then((x) => x.json())
   }
 
   async function fetchItem(id) {
-    return fetch(`${api}/item/${id}.json`).then(x => x.json())
+    return fetch(`${api}/item/${id}.json`).then((x) => x.json())
   }
 
   const total = 30
@@ -25,9 +55,8 @@
 
     await Promise.all(promises)
 
-    promises.forEach(p => {
-      p.then(res => {
-        console.log(res)
+    promises.forEach((p) => {
+      p.then((res) => {
         items.push(res)
       })
     })
@@ -38,16 +67,64 @@
   onMount(fetchData())
 </script>
 
-{#if loaded}
-  {#each items as item }
-    <p>{item.by}</p>
-  {/each}
-{:else}
-  <p>Loading...</p>
-{/if}
+<div id="items">
+  <h1 class="title">{title}</h1>
+  {#if loaded}
+    {#each items as item}
+      <div class="item">
+        <a class="item-title" href={item.url} target="_blank">{item.title}</a>
+        <div class="item-footer">
+          {item.score} points by {item.by}
+          {getAgo(item.time)} ago
+        </div>
+      </div>
+    {/each}
+  {:else}
+    <p>Loading...</p>
+  {/if}
+</div>
 
 <style>
-  p {
-    color: white;
+  #items {
+    padding: var(--global-padding);
+    margin: auto;
+    width: 80vw;
+  }
+
+  .title {
+    color: var(--global-color-white);
+    padding: calc(var(--global-padding) / 2);
+  }
+
+  .item {
+    padding: calc(var(--global-padding) / 2);
+    border-radius: var(--global-border-radius);
+  }
+
+  .item:hover {
+    background: var(--global-color-bg-secondary);
+  }
+
+  .item-title {
+    color: var(--global-color-white);
+    text-decoration: none;
+  }
+
+  .item-title:hover {
+    color: var(--global-color-red);
+    text-decoration: underline;
+  }
+
+  .item-title:visited {
+    color: var(--global-color-dimmed);
+  }
+
+  .item-title:visited:hover {
+    color: var(--global-color-red);
+  }
+
+  .item-footer {
+    color: var(--global-color-dimmed);
+    font-size: 0.8rem;
   }
 </style>
